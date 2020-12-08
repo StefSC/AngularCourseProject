589b90f50ac34bfb5f206f4b40c3ce3951d2c6d8
Stef Serban <serban9slam@yahoo.com>
2020-12-09 00:32:33 +0200

    Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeDetailComponent } from './recipes/recipe-list/recipe-detail/recipe-detail.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    HeaderComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/recipes/dist/recipe.model.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+exports.Recipe = void 0;
+var Recipe = /** @class */ (function () {
+    function Recipe(name, desc, imgPath) {
+        this.name = name;
+        this.description = desc;
+        this.imagePath = imgPath;
+    }
+    return Recipe;
+}());
+exports.Recipe = Recipe;
